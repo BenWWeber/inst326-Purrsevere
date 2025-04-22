5ab22c624fb69227f5e0dc375e2aaae552125308
Ben Weber <benweber@umd.edu>
2025-04-22 18:00:22 -0400

    finished make_deck, modified player_cards to make it more useful for testing

--- make_deck.py
+++ make_deck.py
@@ -1,35 +1,27 @@
 import re
+import random
 
-
+# make_deck description:
 # Creates multiple decks at the beginning of the game that the user can choose 
 # from. Chooses x attack cards, y defense cards, z buff/weakness cards, each 
-# with assigned strength points. Cannot have >1 strong card in each category. 
-# Each deck is assigned a total strength points value, sum of chosen cards 
-# cannot exceed this value. 
+# with assigned strength points. Each deck is assigned a total strength points 
+# value, sum of chosen cards. cannot exceed this value. 
 # 
 # The algorithm will need: 
 #  - Card names, descriptions, and stats, which will be pulled from and created 
 #        algorithmically from a data file
 #  - Specified numbers of cards to put in deck
-#  - Max power level of decks
+#  - Max power level of deck
 
-
-
-# cards: 
+# info on cards: 
 # - attack, defence, buff, debuff
 # - all have accuracy and strength points
 # 
-# damage cards damage come in ranges or tuples with min/max
-# defence cards probably just one number
-# buff and debuff come in %
+# damage cards' magnitude comes in tuples with min/max
+# defence cards' magnitude is just one number
+# buff and debuff cards' magnitude come in %
 #
 # every deck should have at least one attack, one defence, and one buff/debuff
-# not sure how many cards in a deck, probably 4-5
-
-# deck class (not sure we need this over just lists of cards)
-class Deck():
-    def __init__(self):
-        self.cards = list()
 
 # card class
 class Card():
@@ -40,53 +32,133 @@ class Card():
         self.magnitude = magnitude
         self.power_level = power_level
         self.accuracy = accuracy
+        
+    def __str__(self):
+        return (f'Card Name: {self.name}, '
+                + f'Description: {self.description}, '
+                + f'Type: {self.type}, '
+                + f'Magnitude: {self.magnitude}, '
+                + f'Power Level: {self.power_level}, '
+                + f'Accuracy: {self.accuracy}')
+    
 
-
-# deck function (4/18 deliverable)
-def make_deck(path):
+# deck function (4/22 deliverable)
+def make_deck(path, max_count, max_power):
+    deck = list()
+    power = 0
     attacks = list()
     defences = list()
     buffs = list()
-    debuffs = list()
 
     # read in all the player cards and organize them by type
     with open(path, 'r', encoding="utf-8") as file:
         for line in file:
-            card = re.search('?P<name>[^;]+);(?P<description>[^;]+);'
+            card = re.search('(?P<name>[^;]+);(?P<description>[^;]+);'
                               +'(?P<type>[^;]+);(?P<magnitude>[^;]+);'
-                              +'(?P<power_level>[^;]+);(?P<accuracy>[^\n]+',
+                              +'(?P<power_level>[^;]+);(?P<accuracy>[^\n]+)',
                             line)
             
             match card.group('type'):
                 case 'attack':
+                    
+                    mag = card.group('magnitude').split(',')
+                    mag = {int(mag[0]),int(mag[1])}
+                                        
                     attacks.append(Card(card.group('name'),
                                         card.group('description'),
                                         card.group('type'),
-                                        card.group('magnitude'),
-                                        card.group('power_level'),
-                                        card.group('accuracy'),))
+                                        mag,
+                                        float(card.group('power_level')),
+                                        float(card.group('accuracy')),))
                 case 'defence':
                     defences.append(Card(card.group('name'),
                                         card.group('description'),
                                         card.group('type'),
-                                        card.group('magnitude'),
-                                        card.group('power_level'),
-                                        card.group('accuracy'),))
-                case 'buff':
+                                        float(card.group('magnitude')),
+                                        float(card.group('power_level')),
+                                        float(card.group('accuracy')),))
+                case 'buff' | 'debuff':
                     buffs.append(Card(card.group('name'),
                                         card.group('description'),
                                         card.group('type'),
-                                        card.group('magnitude'),
-                                        card.group('power_level'),
-                                        card.group('accuracy'),))
-                case 'debuff':
-                    debuffs.append(Card(card.group('name'),
-                                        card.group('description'),
-                                        card.group('type'),
-                                        card.group('magnitude'),
-                                        card.group('power_level'),
-                                        card.group('accuracy'),))
+                                        float(card.group('magnitude')),
+                                        float(card.group('power_level')),
+                                        float(card.group('accuracy')),))
 
+    # add one random attack and remove from list
+    current_card = attacks.pop(random.randint(0, len(attacks) - 1))
+    power += current_card.power_level
+    deck.append(current_card)
+    
+    # add one random defence and remove from list
+    current_card = defences.pop(random.randint(0, len(defences) - 1))
+    power += current_card.power_level
+    deck.append(current_card)
+    
+    # add one random buff/debuff and remove from list
+    current_card = buffs.pop(random.randint(0, len(buffs) - 1))
+    power += current_card.power_level
+    deck.append(current_card)
+    
+    # put together the card lists
+    remaining_cards = attacks + defences + buffs
+    
+    # sort by power level, highest to lowest
+    remaining_cards.sort(key=lambda s: s.power_level, reverse=True)
+    
+    # add cards until at max power or max card count
+    while max_count > len(deck) and remaining_cards:
+        
+        # remove all cards too strong to fit in the deck
+        while remaining_cards and remaining_cards[0].power_level > (max_power - power):
+            remaining_cards.pop(0)
+
+        if not remaining_cards:
+            break
+        
+        # select card, if last card in the deck add the strongest remaining card
+        if max_count == len(deck) + 1:
+            
+            strongest_cards = [remaining_cards.pop(0)]
+            for card in remaining_cards:
+                if card.power_level == strongest_cards[0].power_level:
+                    strongest_cards.append(card)
+            
+            for card in strongest_cards:
+                print(card.power_level)
+            
+            current_card = strongest_cards.pop(random.randint(0, len(strongest_cards) - 1))
+            power += current_card.power_level
+            deck.append(current_card)
+        else:
+            current_card = remaining_cards.pop(random.randint(0, len(remaining_cards) - 1))
+            power += current_card.power_level
+            deck.append(current_card)
+        
+    return deck
+
+# for testing
+if __name__ == "__main__":
+
+    # make 5 decks
+    decks = list()
+    while len(decks) < 5:
+        decks.append(make_deck('player_cards.txt', 6, 15))
+        
+    # print out the decks
+    for deck in decks:
+        print(f'Deck {decks.index(deck) + 1}:')
+        lvl = 0
+        for card in deck:
+            print(f'\t{card}')
+            lvl += card.power_level
+            
+        print(f'\tTotal deck power level: {lvl}\n')
+            
+    
+    
+            
+            
 
 
     
