93871fe07a9d8bc5f91bffbef8ef5a9fce39b69f
Connor Hall <connor582h@gmail.com>
2025-05-06 15:07:24 -0400

    now takes separate cat and human decks

--- deck_selection.py
+++ deck_selection.py
@@ -1,14 +1,12 @@
 import random 
 
-def deck_selection(decks):
-    for index, deck in enumerate(decks): # go through each deck
+def deck_selection(human_decks, cat_decks):
+    for index, deck in enumerate(human_decks): # go through each deck
             # print deck number in list
         print(f'Deck {deck + 1}')
         for card in deck:
             print(card)
 
-        user_input = int(input("Choose your deck: ") - 1)
-        # input validation
-        human_selection = decks.pop(user_input)
-        computer_selection = random.choice(decks)
+        human_selection = int(input("Choose your deck: ") - 1)
+        computer_selection = random.choice(cat_decks)
         return human_selection, computer_selection
