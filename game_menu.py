1d5e69ebddbc33aeed76eb6c57c1560d7b4020bd
Connor Hall <connor582h@gmail.com>
2025-05-06 18:01:15 -0400

    created game menu function

---
+++ game_menu.py
@@ -0,0 +1,21 @@
+def game_menu(deck):
+    # prints game options
+
+    option = int(input("Menu:\
+        \nOption 1: select card\
+        \nOption 2: print your deck\
+        \nSelect option: "))
+    # 3rd option: print opponent's deck
+    # input validation
+    
+    while option != 1:
+        if option == 2:
+            for card in deck:
+                print(card) 
+        option = int(input("Select option: "))
+    selection = int(input(f"Select card 1-{len(deck)}: "))  
+    
+    return selection
+    
+   
+        
