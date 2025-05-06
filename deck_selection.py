b0e175459ad986d5fa1fabc75650fd8401a701b9
Connor Hall <connor582h@gmail.com>
2025-05-06 15:24:46 -0400

    minor fixes

--- deck_selection.py
+++ deck_selection.py
@@ -3,10 +3,10 @@ import random
 def deck_selection(human_decks, cat_decks):
     for index, deck in enumerate(human_decks): # go through each deck
             # print deck number in list
-        print(f'Deck {deck + 1}')
+        print(f'Deck {index + 1}')
         for card in deck:
             print(card)
 
-        human_selection = int(input("Choose your deck: ") - 1)
-        computer_selection = random.choice(cat_decks)
-        return human_selection, computer_selection
+    human_selection = int(input("Choose your deck: ") - 1)
+    computer_selection = random.choice(cat_decks)
+    return human_selection, computer_selection
