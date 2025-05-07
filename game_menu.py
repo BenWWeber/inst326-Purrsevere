1bc75acc7dce63a745d9359239e3b7a21d8a3a8f
Hagan Yeoh <haganyhk99@gmail.com>
2025-05-06 20:35:40 -0400

    adding game_menu

--- game_menu.py
+++ game_menu.py
@@ -1,18 +1,22 @@
-def game_menu(deck):
+def game_menu(deck, player, cat):
     # prints game options
 
     option = int(input("Menu:\
         \nOption 1: select card\
-        \nOption 2: print your deck\
+        \nOption 2: show your deck\
+        \nOption 3: show stats\
         \nSelect option: "))
     # 3rd option: print opponent's deck
     # input validation
     
     while option != 1:
         if option == 2:
+            print()
             for card in deck:
                 print(card)
-            print() 
+            print()
+        elif option == 3:
+            print(f"\n{player}\n{cat}\n")
         option = int(input("Select option: "))
     selection = int(input(f"Select card 1-{len(deck)}: ")) - 1 
     
