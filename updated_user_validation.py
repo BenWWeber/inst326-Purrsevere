cfb0c9c8426aa653b1b1e2c1bbffcde175f6ac86
Jadon Tallapally <jadontallapally27@gmail.com>
2025-05-08 18:07:33 -0400

    updated user input function that is more generic

---
+++ updated_user_validation.py
@@ -0,0 +1,15 @@
+def validate_user_input(input_type, data_type=str, allowed_values=None):
+    while True:
+        response = input(input_type)
+        try:
+            input_value = response.lower() if data_type is str else response
+            converted_value = input_value if data_type is str else data_type(input_value)
+        
+        except ValueError:
+            print(f"Please enter a valid {data_type.__name__}")
+            continue
+        
+        if allowed_values is not None and converted_value not in allowed_values:
+            print(f"Input Invalid! Try again!")
+            continue
+        return converted_value         
