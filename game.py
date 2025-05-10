863879f92a7e83d5ca612cd2e58da6d8231c737c
Connor Hall <connor582h@gmail.com>
2025-05-10 19:52:28 -0400

    rewrote computer_card_draw docstring

--- game.py
+++ game.py
@@ -34,6 +34,7 @@ class Player:
             f"Defense Multiplier = {round(self.defense_multiplier, 2)}"
         )
 
+
 def resolve_attack(card_accuracy, damage_range, user_multiplier=1.0, 
                    defender_multiplier=1.0):
     card_accuracy *= 100
@@ -50,6 +51,7 @@ def resolve_attack(card_accuracy, damage_range, user_multiplier=1.0,
 
     damage = int(damage * user_multiplier / defender_multiplier)
     return True, damage
+
 
 def apply_card_effect(card, user, target):
     landed = True
@@ -90,6 +92,7 @@ def apply_card_effect(card, user, target):
     else:
         print(f"Unknown card type: {card.type}")
 
+
 def computer_card_draw(owner_hp, cat_hp, cat_deck, owner_deck, cat):
     """Determines which card the computer (cat) draws. Prioritizes defense
     (if computer can be defeated in one turn), then attack (if owner can be
@@ -98,8 +101,9 @@ def computer_card_draw(owner_hp, cat_hp, cat_deck, owner_deck, cat):
     Args: 
         owner_hp (int): health points of the owner (player)
         cat_hp (int): health points of the cat (computer)
-        cat_cards (list): list of cat's card objects
-        owner_cards (list): list of owner's card objects
+        cat_deck (list): list of cat's card objects
+        owner_deck (list): list of owner's card objects
+        cat (Player object): Player object representation of the cat
                         
     Side effects:
         None
@@ -111,7 +115,7 @@ def computer_card_draw(owner_hp, cat_hp, cat_deck, owner_deck, cat):
     cat_powerups = [card for card in cat_deck if card.type[-4:] == 'buff']
     owner_attacks = [card for card in owner_deck if card.type == 'attack']
     
-    #defend when owner's (player) attack can defeat cat (computer)
+    # raise defense when owner is close to defeating the cat
     cat_defense = [card for card in cat_powerups if card.type == 'defense buff']
     if cat.fearCount == 1:
         if len(cat_defense) > 0:
@@ -122,18 +126,18 @@ def computer_card_draw(owner_hp, cat_hp, cat_deck, owner_deck, cat):
                     print("Cat is afraid! He enrages and shows his meow-scles!")
                     return cat_defense[0]
             
-    #draw attack card if can defeat owner (player)    
+    #draw attack card if can defeat owner 
     #chooses strongest possible attack for increased chance of winning 
     cat_attacks.sort(key=lambda c: max(c.magnitude), reverse=True)
     if max(cat_attacks[0].magnitude) >= owner_hp:
         return cat_attacks[0]
         
     #choose between attack and powerup, greater chance of attack
-    #randomly chooses attack
     if random.random() < 0.7 or len(cat_powerups) == 0:
         return random.choice(cat_attacks)
     else:
         return random.choice(cat_powerups)
+
 
 if __name__ == "__main__":
     '''
