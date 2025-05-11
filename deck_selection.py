29b74c6f7e497e853a410f20ec94b33d3d8678e9
Connor Hall <connor582h@gmail.com>
2025-05-10 20:09:18 -0400

    added docstring

--- deck_selection.py
+++ deck_selection.py
@@ -1,11 +1,22 @@
 import random 
 from input_validation import validate_input 
 
-def deck_selection(human_decks, cat_decks):
+def deck_selection(owner_decks, cat_decks):
+    ''' prints decks to choose from, takes user input and chooses cat's deck
     
+    Args:
+        owner_decks (list): list of lists containing owner Card objects
+        cat_decks (list): list of lists containing cat Card objects
+        
+    Side effects:
+        prints to stdout
+    
+    Returns:
+        2 lists of Card objects for the owner and the cat
+    '''
     valid_inputs = list()
     
-    for index, deck in enumerate(human_decks): # go through each deck
+    for index, deck in enumerate(owner_decks): # go through each deck
         # print deck number in list
         print(f'Deck {index + 1}')
         valid_inputs.append(index + 1)
@@ -15,6 +26,6 @@ def deck_selection(human_decks, cat_decks):
         print()
     
     input = validate_input("Choose your deck: ", data_type=int, allowed_values=valid_inputs)
-    human_selection = int(input) - 1
-    computer_selection = random.choice(cat_decks)
-    return human_selection, computer_selection
+    owner_selection = int(input) - 1
+    cat_selection = random.choice(cat_decks)
+    return owner_selection, cat_selection
