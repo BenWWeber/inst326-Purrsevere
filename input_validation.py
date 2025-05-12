cc795ffce8f0e06926c6d0d8645827654b77076e
Jadon Tallapally <jadontallapally27@gmail.com>
2025-05-11 20:00:59 -0400

    Added doc string

--- input_validation.py
+++ input_validation.py
@@ -1,4 +1,28 @@
 def validate_input(input_type, data_type=str, allowed_values=None):
+    """ generic input validation for all types of user input 
+    
+    Primary Author: Jadon Tallapally
+    
+    Args:
+        input_type(str or int): Any type of input that the game requests from 
+                the user
+        data_type(str or int): The data type to convert the input into
+                (default = str)
+        allowed_values(list or set): A collection of valid values that the given 
+            input have to match (default = None)
+       
+    Returns:
+        converted_value(str or int): The validated and converted input value that 
+        matches the specified data_type
+        
+    Side Effects:
+        prints to the console error messages if input is invalid:
+        if the data type is invalid (if valueError is caught), it prints 
+            "Please enter a valid (data type)"
+        if the data type is not in the collection, it prints "Input Invalid! 
+            Try again!"
+        
+    """
     while True:
         response = input(input_type)
         try:
